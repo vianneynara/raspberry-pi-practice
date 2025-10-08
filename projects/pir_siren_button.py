3b4afe225c4de8b755285db2f733deccd57f89b4
vianneynara <narwandaru.nara@gmail.com>
2025-10-08 13:42:32 +0700

    fix: alarm with buzzer output
    Took 14 minutes

---
+++ projects/pir_siren_button.py
@@ -0,0 +1,49 @@
+from time import sleep
+
+import gpiozero
+from signal import pause
+
+# Setup LED on GPIO pin 14
+led = gpiozero.LED(
+    pin=14
+)
+motion_sensor = gpiozero.MotionSensor(
+    pin=15
+)
+
+buzzer = gpiozero.Buzzer(pin=18)
+# buzzer = gpiozero.TonalBuzzer(pin=18)
+
+button = gpiozero.Button(
+    pin=3
+)
+
+alarm_is_on = False
+
+print("Menunggu gerakan")
+
+def motion_detected():
+    global alarm_is_on
+    alarm_is_on = True
+    print("Motion detected!")
+    while alarm_is_on:
+        buzzer.on()
+        led.on()
+        sleep(0.1)
+        buzzer.off()
+        led.off()
+        sleep(0.1)
+
+def button_pressed():
+    global alarm_is_on
+    print("Button pressed! Turning alarm off.")
+    alarm_is_on = False
+
+
+motion_sensor.when_motion = motion_detected
+button.when_pressed = button_pressed
+
+try:
+    pause()
+except KeyboardInterrupt:
+    print("Menghentikan program")
